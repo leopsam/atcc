0331a04679be6969f9d514d4b37dc786643cd7e2
Leonardo Pereira Sampaio <leonardo.lps@outlook.com.br>
2024-10-09 20:13:25 -0300

    add loading in student

--- src/app/student/layout.js
+++ src/app/student/layout.js
@@ -1,10 +1,12 @@
 import Navbar from './navbar/page'
+import { Suspense } from 'react'
+import Loading from './loading'
 
 export default function StudentLayout({ children }) {
     return (
         <main>
             <Navbar />
-            {children}
+            <Suspense fallback={<Loading />}>{children}</Suspense>
         </main>
     )
 }
